9322bc23252c19a881e9b45f52c15cfdc7188cf4
git <git@gitlab.pro>
2013-11-07 16:58:11 +0800

    add: v1.1.2

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # jQuery Cookie Plugin
 
-A jQuery plugin for cookie control and support multiple cookies combination.
+A jQuery plugin for cookie control and support multiple cookies combination, and you can use it even without the jQuery library.
 
 ## Compatibility
 
@@ -12,6 +12,10 @@ A jQuery plugin for cookie control and support multiple cookies combination.
 
 Require the plugin:
 
+```
+<script tpye="text/javascript" src='jquery.cookie.js'></script>
+```
+In FIS:
 ```
 require('common:widget/ui/jquery/jquery.cookie.js');
 ```
@@ -73,13 +77,13 @@ Read cookie from the combined one:
 $.cookie.get('the_cookie');
 ```
 
-Move a individual cookie into the combined one:
+Move a individual cookie into the combined one (NOT AVAILABLE):
 
 ```
 $.cookie.move('the_cookie');
 ```
 
-Move a individual expiring cookie into the combined one, 7 days from then:
+Move a individual expiring cookie into the combined one, 7 days from then (NOT AVAILABLE):
 
 ```
 $.cookie.move('the_cookie', { expires: 7 });
@@ -91,7 +95,7 @@ Remove a specified cookie from the combined one:
 $.cookie.del('the_cookie');
 ```
 
-Clear the whole combined cookie:
+Clear the whole combined cookie (NOT AVAILABLE):
 
 ```
 $.cookie.clear();
@@ -99,8 +103,16 @@ $.cookie.clear();
 
 ## Release History
 
-* 2012/06/01 - v1.0.0
-    - First released version, all basic features supported.
+* 2013/11/07 - v1.1.2
+    - Redeclare jQuery namespace, in order to use jQuery.cookie() without jQuery library.
+    - Change the logic of cookie overflow. Now you cannot write the cookie when it's overflow.
+    - Add callback function named 'error' into 'set' function's options, it will be called when writing a cookie and it is overflow.
+    - Simplify jQuery.cookie.set function's logic.
+    - Commented move and clear functions for safe.
+    - Some other minor changes and improvements.
+* 2013/10/22 - v1.1.1
+    - Update the comments.
+    - Update set function's logic.
 * 2013/08/28 - v1.1.0
     - By using the default cookie function, set it as session cookie if not defined the expire time.
     - By using get/move function, set the expire time in 2000 days.
@@ -109,9 +121,8 @@ $.cookie.clear();
     - Convert the expire time in combined cookie into duotricemary notation.
     - If a cookie has been updated, move it to the first place of the combined data.
     - Fixed some bugs.
-* 2013/10/22 - v1.1.1
-    - Update the comments.
-    - Update set function's logic.
+* 2012/06/01 - v1.0.0
+    - First released version, all basic features supported.
 
 ## Authors
 
--- src/jquery.cookie.js
+++ src/jquery.cookie.js
@@ -1,26 +1,15 @@
 /**
-* @des    cookie
-* @author 斯人
-* @date 2012-06-01
-* @upate [{luis|迁移到jquery基础类库|2012-06-01},{author|dex|date}]
-* @api  cookie存取的第一种方法：$.cookie(key,value)和$.cookie(key)，原始cookie存取，不合并
-*                               $.cookie.set(key,value)和$.cookie.get(key);合并cookie的存取
-
-**
-* V1.1
+* jQuery Cookie Plugin
 * @Frank.F
-* @2013.8.28
+* V1.1.2
 * @Change log: 
-*   1. By using the default cookie function, set it as session cookie if not defined the expire time.
-*   2. By using get/move function, set the expire time in 2000 days.
-*   3. Auto delete the last cookie when the combined one is too long (larger than 2k).
-*   4. Set defult path as blank.
-*   5. Convert the expire time in combined cookie into duotricemary notation.
-*   6. If a cookie has been updated, move it to the first place of the combined data.
-*   7. Fixed some bugs.
-* @2013.10.22
-*   1. Update the comments.
-*   2. Update set function's logic.
+* @2013.11.07
+*   1. Redeclare jQuery namespace, in order to use jQuery.cookie() without jQuery library.
+*   2. Change the logic of cookie overflow. Now you cannot write the cookie when it's overflow.
+*   3. Add callback function named 'error' into 'set' function's options, it will be called when writing a cookie and it is overflow.
+*   4. Simplify jQuery.cookie.set function's logic.
+*   5. Commented move and clear functions for safe.
+*   6. Some other minor changes and improvements.
 
 * @Usage: 
 *   Original jQuery Cookie Set: $.cookie(key, value[, option]);
@@ -28,156 +17,188 @@
 *   Combined Cookie Set: $.cookie.set(key, value[, expires]);
 *   Combined Cookie Get: $.cookie.set(key);
 */
+
+window.jQuery || (window.jQuery = {});
+
 ;;(function ($, DOC, NAME) {
 
-    var cookieNamePre = "HCD_", // Cookie name's prefix
-    cookieNameSuffix = 0, // Cookie data's number
-    curCookieName = cookieNamePre + cookieNameSuffix, // Current cookie name
-    ENCODE = encodeURIComponent,
-    DECODE = decodeURIComponent,
-    NULL = null,
+    var COOKIENAME = "HCD", // Current cookie name
+        ENCODE = encodeURIComponent,
+        DECODE = decodeURIComponent,
+        NULL = null,
+        MAXLENGTH = 2048,   // Max length of the whole cookie data
+        EXPIREDAYS = 2000,  // The combined cookie's expired days
 
-    /**
-     * Whether a string is not empty
-     * @param  {String} o  String value
-     * @return {Boolean}   True is not empty; false is empty
-     */
-    notEmpty = function (o) {
-        return o + "" === o && o !== "";
-    },
+        /**
+         * Whether a string is not empty
+         * @param  {String} o  String value
+         * @return {Boolean}   True is not empty; false is empty
+         */
+        notEmpty = function (o) {
+            return o + "" === o && o !== "";
+        },
 
-    /**
-     * Convert expire time's format from day to millisecond
-     * @param  {Number} expires Expire days
-     * @return {String}         Expire time in duotricemary notation
-     */
-    toTime = function (expires) {
-        var time = expires == 0 ? 0 : ((new Date).getTime() + expires * 86400000);
-        // convert to duotricemary notation
-        return time.toString(32);
-    },
+        /**
+         * Convert expire time's format from day to millisecond
+         * @param  {Number} expires Expire days
+         * @return {String}         Expire time in duotricemary notation
+         */
+        toTime = function (expires) {
+            var time = expires == 0 ? 0 : ((new Date).getTime() + expires * 86400000);
+            // convert to duotricemary notation
+            return time.toString(32);
+        },
 
-    /**
-     * Convert expire time UTC string in millisecond
-     * @param  {Number} expires Expire days
-     * @return {String}         UTC String of the expire time
-     */
-    toExpires = function (expires) {
-        if (expires == 0) return "";
-        var date = new Date;
-        // conver to decimal system
-        date.setTime(parseInt(toTime(expires), 32));
-        return date.toUTCString();
-    },
+        /**
+         * Convert expire time UTC string in millisecond
+         * @param  {Number} expires Expire days
+         * @return {String}         UTC String of the expire time
+         */
+        toExpires = function (expires) {
+            if (expires == 0) return "";
+            var date = new Date;
+            // conver to decimal system
+            date.setTime(parseInt(toTime(expires), 32));
+            return date.toUTCString();
+        },
 
-    /**
-     * Set cookie (original usage)
-     * @param {String} name   Cookie's name
-     * @param {String|Number} value  Cookie's value
-     * @param {Object} Option options
-     */
-    set = function (name, value, option) {
+        /**
+         * Set cookie (original usage)
+         * @param {String} name   Cookie's name
+         * @param {String|Number} value  Cookie's value
+         * @param {Object} Option options
+         */
+        set = function (name, value, option) {
 
-        option = option || {};
+            option = option || {};
 
-        //session cookie as default
-        var expires = Number(option.expires);
+            //session cookie as default
+            var expires = Number(option.expires);
 
-        // delete the cookie data while value is equal to null
-        if (value === null) {
-            expires = -1;
-        }
+            // delete the cookie data while value is equal to null
+            if (value === NULL) {
+                expires = -1;
+            }
 
-        DOC.cookie = [
-            name, '=',
-            value,
-            expires ? '; expires=' + toExpires(expires) : '',
-            notEmpty(option.domain) ? '; domain=' + option.domain : '',
-            notEmpty(option.path) ? '; path=' + option.path : '',
-            option.secure ? '; secure' : ''
-        ].join('');
-        name + '=' + value + option.expires + option.domain + option.path + option.secure;
-    },
+            // overflow judgement
+            if (expires !== -1) {
+                var newCookie = DOC.cookie,
+                    matchedCookie = newCookie.match('(?:^|; )' + name + '(?:(?:=([^;]*))|;|$)');
 
-    /**
-     * Get cookie (original usage)
-     * @param  {String} name Cookie's name
-     * @return {String}      Cookie's value
-     */
-    get = function (name) {
-        var result = notEmpty(name) ? DOC.cookie.match('(?:^| )' + name + '(?:(?:=([^;]*))|;|$)') : null;
-        return result ? result[1] : result;
-    },
+                // imitate the new cookie data before writing
+                if (matchedCookie) {
+                    newCookie = /^;/.test(matchedCookie) ? newCookie.replace(matchedCookie[0], '; ' + name + '=' + value) : newCookie.replace(matchedCookie[0], name + '=' + value);
+                } else {
+                    newCookie += '; ' + name + '=' + value;
+                }
 
-    /**
-     * Match data with name, in the format of 'name|value|time'
-     * @param  {String} name 
-     * @param  {String} data 
-     * @return {String}      Matched result
-     */
-    matchAll = function (name, data) {
-        var result = (data || "").match('(?:^|,)(' + ENCODE(name) + '\\|[^,]+)');
-        return result && result[1];
-    },
+                if (newCookie.length > MAXLENGTH) {
+                    // error callback
+                    option.error && option.error();
+                    // refuse to write cookie when it's overflow
+                    return false;
+                }
+            }
 
-    /**
-     * Check whether the data is overflow and if do so, delete the last one
-     * @param  {String} data   
-     * @param  {Number} length Limited length
-     * @return {String}        Result data value
-     */
-    checkLength = function (data, length) {
-        if (data.length > length) {
-            data = data.split(",");
-            data.pop();
-            data = data.join(",");
-            data = checkLength(data, length);
-            return data;
-        } else {
-            return data;
-        }
-    },
+            // Write cookie
+            DOC.cookie = [
+                name, '=',
+                value,
+                expires ? '; expires=' + toExpires(expires) : '',
+                notEmpty(option.domain) ? '; domain=' + option.domain : '',
+                notEmpty(option.path) ? '; path=' + option.path : '',
+                option.secure ? '; secure' : ''
+            ].join('');
+        },
 
-    /**
-     * $.cookie() function's entrance
-     * @param  {String} name   Cookie's name
-     * @param  {String|Number} value  Cookie's value, call get func while is empty, otherwise call set func.
-     * @param  {Object} option Options
-     * @return {[type]}        Set or get cookie function's return value
-     */
-    _ = function (name, value, option) {
-        return (value || value === NULL || value === "") ? set(ENCODE(name), ENCODE(value), option) : (get(ENCODE(name)) ? DECODE(get(ENCODE(name))) : null);
-    };
+        /**
+         * Get cookie (original usage)
+         * @param  {String} name Cookie's name
+         * @return {String}      Cookie's value
+         */
+        get = function (name) {
+            var result = notEmpty(name) ? DOC.cookie.match('(?:^| )' + name + '(?:(?:=([^;]*))|;|$)') : NULL;
+            return result ? result[1] : result;
+        },
+
+        /**
+         * Match data with name, in the format of 'name|value|time'
+         * @param  {String} name 
+         * @param  {String} data 
+         * @return {String}      Matched result
+         */
+        matchAll = function (name, data) {
+            var result = (data || "").match('(?:^|,)(' + ENCODE(name) + '\\|[^,]+)');
+            return result && result[1];
+        },
+
+        /**
+         * Check whether the data is overflow and if do so, delete the last one
+         * @param  {String} data   
+         * @param  {Number} length Limited length
+         * @return {String}        Result data value
+         */
+        /*checkLength = function (data, length) {
+            if (data.length > length) {
+                data = data.split(",");
+                data.pop();
+                data = data.join(",");
+                data = checkLength(data, length);
+                return data;
+            } else {
+                return data;
+            }
+        },*/
+
+        /**
+         * $.cookie() function's entrance
+         * @param  {String} name   Cookie's name
+         * @param  {String|Number} value  Cookie's value, call get func while is empty, otherwise call set func.
+         * @param  {Object} option Options
+         * @return {[type]}        Set or get cookie function's return value
+         */
+        _ = function (name, value, option) {
+            return (value || value === NULL || value === "") ? set(ENCODE(name), ENCODE(value), option) : (get(ENCODE(name)) ? DECODE(get(ENCODE(name))) : NULL);
+        };
 
     /**
      * Set cookie into the combined one, you can set expires only, the other options are default as {domain:"", path:"/", secure:""}
      * @param {String} name    Cookie's name
      * @param {String|Number} value   Cookie's value
-     * @param {Number} expires Cookie's expire days
+     * @param {Object} option Options
      */
-    _.set = function (name, value, expires) {
+    _.set = function (name, value, option) {
+
+        option = option || {};
 
         //anyway, delete the original cookie data
-        set(ENCODE(name), null);
+        set(ENCODE(name), NULL);
 
-        var result = get(curCookieName) || "",
+        var expires = option.expires,
 
-        //old data
-        matchData = matchAll(name, result) || "";
+            result = get(COOKIENAME) || "",
+
+            //old data
+            matchData = matchAll(name, result) || "";
 
         // remove the original cookie and set the new data as the first place
-        if ((new RegExp("^" + matchData.replace(/\|/g, "\\|"), "g")).test(result)) {
+        /*if ((new RegExp("^" + matchData.replace(/\|/g, "\\|"), "g")).test(result)) {
             var newData = result.replace(matchData, [ENCODE(name), ENCODE(value), toTime(expires || 0)].join("|") + (result.length && !matchData ? "," : ""));
         } else {
             var reg = new RegExp(",?" + matchData.replace(/\|/g, "\\|"), "g"),
                 newData = ([ENCODE(name), ENCODE(value), toTime(expires || 0)].join("|") + (result.length ? "," : "")).concat(result.replace(reg, ""));
+        }*/
+
+        // remove the original cookie and get the new entire HCD cookie data
+        if (new RegExp(matchData.replace(/\|/g, "\\|"), "g").test(result)) {
+            var newData = result.replace(matchData, [ENCODE(name), ENCODE(value), toTime(expires || 0)].join("|") + (result.length && !matchData ? "," : ""));
         }
 
         // if the data's length is overflow, then delete the last one
-        newData = checkLength(newData, 2048);
+        // newData = checkLength(newData, MAXLENGTH);
 
         //expires ==> timestamp
-        set(curCookieName, newData, { expires: 2000, domain: '' });
+        set(COOKIENAME, newData, { expires: EXPIREDAYS, domain: '', error: option.error });
 
         return _;
     };
@@ -190,9 +211,9 @@
     _.get = function (name) {
         //timestamp
         var _data = get(ENCODE(name));
-        if (_data !== null) return DECODE(_data);
+        if (_data !== NULL) return DECODE(_data);
 
-        var data = get(curCookieName) || "",
+        var data = get(COOKIENAME) || "",
         matchData = matchAll(name, data);
 
         //null
@@ -205,19 +226,19 @@
 
         //delete when the cookie is exist but expired
         _.del(ENCODE(name));
-        return null;
+        return NULL;
     };
 
     /**
-     * Migrate the old cookie data into the combined one
+     * COMMENTED: Migrate the old cookie data into the combined one
      * @param  {String} name    Cookie's name which need to be migrated
      * @param  {Number} expires Expire days
      */
-    _.move = function (name, expires) {
+    /*_.move = function (name, expires) {
         var value = get(ENCODE(name));
-        value !== null && _.set(name, DECODE(value), expires || 365);
+        value !== NULL && _.set(name, DECODE(value), expires || 365);
         return _;
-    }
+    };*/
 
     /**
      * Delete a cookie from the combined one
@@ -225,20 +246,20 @@
      */
     _.del = function (name) {
         //anyway, kill old
-        set(ENCODE(name), null);
+        set(ENCODE(name), NULL);
 
-        var data = get(curCookieName) || "",
+        var data = get(COOKIENAME) || "",
         matchData = matchAll(name, data) || "",
         matchReg = new RegExp(",?" + matchData.replace(/\|/g, "\\|") + ",?", "g");
-        matchData && set(curCookieName, data.replace(matchReg, ""));
+        matchData && set(COOKIENAME, data.replace(matchReg, ""));
         return _;
-    }
+    };
 
-    /** Clear the combined cookie */
-    _.clear = function () {
-        set(curCookieName, "");
+    /** COMMENTED: Clear the combined cookie */
+    /*_.clear = function () {
+        set(COOKIENAME, "");
         return _;
-    }
+    };*/
 
     _.is = !!navigator.cookieEnabled;
 
